002b22f8568b08d1746195d79c75e120dbcb3648
99002477 <71481753+99002477@users.noreply.github.com>
2020-11-10 14:14:53 +0530

    Update client.py

--- Implementation/src/client.py
+++ Implementation/src/client.py
@@ -3,29 +3,19 @@ from socket import *;
 import speech_recognition as sr
 client_socket = socket(AF_INET, SOCK_STREAM);
 client_socket.connect(("127.0.0.1",9999));
-
-
 # Initialize recognizer class (for recognizing the speech)
 r = sr.Recognizer()
-
 # Reading Audio file as source
 # listening the audio file and store in audio_text variable
-
 with sr.AudioFile('python_audio.wav') as source:
-    
-    audio_text = r.listen(source)
-    
+audio_text = r.listen(source)
 # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
-    try:
-        
+    try:     
         # using google speech recognition
         text = r.recognize_google(audio_text)
-        print('Sharing the audio content to server..')
-        
-     
+        print('Sharing the audio content to server..') 
     except:
          print('Sorry.. run again...')
-		 
 recordfile=open("recordtext.txt","w")
 recordfile.write(text);
 recordfile.close();
