9ac798ac79a0034518de98db6d3e445e6f655943
99002477 <71481753+99002477@users.noreply.github.com>
2020-11-10 14:16:18 +0530

    Update server.py

--- Implementation/src/server.py
+++ Implementation/src/server.py
@@ -1,6 +1,4 @@
 import socket;
-import string;
-import re;
 from socket import *;
 server_socket = socket(AF_INET, SOCK_STREAM);
 server_socket.bind(("127.0.0.1",9999));
@@ -9,6 +7,5 @@ print("Waiting for Connection...");
 connection, addr = server_socket.accept();
 data=connection.recv(1024);
 data = data.decode();
-
 print("Message is : \n",data);
 server_socket.close();
